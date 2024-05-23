4c2b07d9cf6cd51fe9985fbbc3b96e2084f1a422
chughkirat06 <chughkirat06@gmail.com>
2024-05-22 20:19:39 -0400

    P1: Age group categorization

--- Conditional Programs/solution01.py
+++ Conditional Programs/solution01.py
@@ -8,3 +8,13 @@ Adult (20-59),
 Senior (60+)
 """
 
+user_age = int(input("Provide an age: "))
+
+if user_age < 13:
+    print("Child")
+elif user_age < 20:
+    print("Teenager")
+elif user_age < 60:
+    print("Adult")
+else:
+    print("Senior")
