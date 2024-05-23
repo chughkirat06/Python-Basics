c8b832fe8dfed502403de8fd5745554c3a805ae1
chughkirat06 <chughkirat06@gmail.com>
2024-05-22 20:06:08 -0400

    Initial commit

---
+++ Conditional Programs/solution01.py
@@ -0,0 +1,13 @@
+# Age group categorization
+
+"""
+Classify a person's age group:
+Child (< 13),
+Teenager (13-19),
+Adult (20-59),
+Senior (60+)
+"""
+
+user_age = int(input("Please enter your age: "))
+
+if user_age > 
