c45cb9d0406f81299750b35fc622138607716732
chughkirat06 <chughkirat06@gmail.com>
2024-05-24 17:12:57 -0400

    P1: Counting positive numbers

---
+++ Loop Programs/solution01.py
@@ -0,0 +1,14 @@
+# Counting positive numbers
+
+"""
+Given a list of numbers, count how many are positive.
+"""
+
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+count = 0
+
+for number in numbers:
+    if number > 0:
+        count += 1
+
+print(count)
