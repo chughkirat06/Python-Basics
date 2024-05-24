b6d16da9c4ac2cd1dd1734e9a4b148ba70489534
chughkirat06 <chughkirat06@gmail.com>
2024-05-24 19:51:14 -0400

    P9: List uniqueness checker

---
+++ Loop Programs/solution09.py
@@ -0,0 +1,16 @@
+# List uniqueness checker
+
+"""
+Check if all elements in a list are unique.
+If a duplicate is found, exit the loop and print the duplicate.
+"""
+
+items = ["apple", "banana", "orange", "apple", "mango"]
+
+unique_item = set()
+
+for item in items:
+    if item in unique_item:
+        print("Duplicate item is: ", item)
+        break
+    unique_item.add(item)
