0934422014883746c70bb9776084939b852cd0c8
chughkirat06 <chughkirat06@gmail.com>
2024-05-25 21:24:38 -0400

    P10: Recursive factorial

---
+++ Functional Programs/solution10.py
@@ -0,0 +1,13 @@
+# Recursive function
+
+"""
+Create a recursive function to calculate the factorial of a number.
+"""
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print(factorial(5))
