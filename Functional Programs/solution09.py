850184237245cd2b61affff4d73f588e001ec4de
chughkirat06 <chughkirat06@gmail.com>
2024-05-25 21:14:16 -0400

    P9: Generator function with yield

---
+++ Functional Programs/solution09.py
@@ -0,0 +1,12 @@
+# Generator function with yield
+
+"""
+Write a generator function that yields even numbers up to a specified limit.
+"""
+
+def even_generator(limit):
+    for i in range(2, limit + 1, 2):
+        yield i
+    
+for num in even_generator(10):
+    print(num)
